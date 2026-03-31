6d018e7acb045fbbd575c8a01a959d469dc420ea
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-03-31 20:56:04 +0530

    Added solution for rotating matrix 90 degrees clockwise using transpose and row reversal #Problem 48

---
+++ RotateImage.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class RotateImage {
+
+    public void rotate(int[][] matrix) {
+
+        int n = matrix.length;
+
+        // Transpose
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+
+            }
+        }
+
+        // Reverse rows
+        for (int i = 0; i < n; i++) {
+
+            int left = 0, right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+    }
+
+    //  main method class 
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        RotateImage obj = new RotateImage();  // 
+        obj.rotate(matrix);
+
+        // Print result
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+}
