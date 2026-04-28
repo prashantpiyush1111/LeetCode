d823e1fc3573cca3f9eeb730a7f8edbb0d5db9dc
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-04-28 23:25:25 +0530

    implement binary search for search insert position with main method for execution

---
+++ SearchInsertPosition.java
@@ -0,0 +1,30 @@
+public class SearchInsertPosition {
+
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } 
+            else if (nums[mid] > target) {
+                right = mid - 1;
+            } 
+            else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        int target = 5;
+
+        int result = searchInsert(nums, target);
+        System.out.println("Insert Position: " + result);
+    }
+}
