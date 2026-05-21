2a2cc013df4953f3151a1df11a0db9df597cbe11
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-05-21 23:28:02 +0530

    refactor: optimize Add Strings solution with static methods and multiple test cases

--- AddStringsSolution.java
+++ AddStringsSolution.java
@@ -1,32 +1,30 @@
 public class AddStringsSolution {
-    public String addStrings(String num1, String num2) {
-        StringBuilder str = new StringBuilder();
 
+    public static String addStrings(String num1, String num2) {
+        StringBuilder result = new StringBuilder();
         int i = num1.length() - 1;
         int j = num2.length() - 1;
         int carry = 0;
-        int sum;
 
-        while (i >= 0 || j >= 0 || carry > 0) {
-            int digit1 = (i >= 0) ? num1.charAt(i--) - '0' : 0;
-            int digit2 = (j >= 0) ? num2.charAt(j--) - '0' : 0;
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int digit1 = i >= 0 ? num1.charAt(i--) - '0' : 0;
+            int digit2 = j >= 0 ? num2.charAt(j--) - '0' : 0;
 
-            sum = digit1 + digit2 + carry;
-            str.append(sum % 10);
-            carry = sum / 10;
+            int total = digit1 + digit2 + carry;
+            result.insert(0, total % 10); // Changed: insert at beginning instead of append + reverse
+            carry = total / 10;
         }
 
-        return str.reverse().toString();
+        return result.toString();
+    }
+
+    public static void testCase(String a, String b) { // Changed: helper method for multiple test cases
+        System.out.println(a + " + " + b + " = " + addStrings(a, b));
     }
 
     public static void main(String[] args) {
-        AddStringsSolution solution = new AddStringsSolution();
-
-        String num1 = "456";
-        String num2 = "77";
-
-        String result = solution.addStrings(num1, num2);
-
-        System.out.println("Sum: " + result);
+        testCase("123", "789");
+        testCase("999", "1");
+        testCase("456", "77");
     }
 }
