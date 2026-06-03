a93398f0b6c98d901e9275e5e83e7459660708b2
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-06-03 23:49:32 +0530

    Add SudokuValidator implementation using HashSet for row, column, and box validation

---
+++ SudokuValidator.java
@@ -0,0 +1,58 @@
+import java.util.HashSet;
+
+public class SudokuValidator {
+
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[] rows = new HashSet[9];
+        HashSet<Character>[] cols = new HashSet[9];
+        HashSet<Character>[] boxes = new HashSet[9];
+
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+
+                if (board[r][c] == '.') {
+                    continue;
+                }
+
+                char value = board[r][c];
+                int boxIndex = (r / 3) * 3 + (c / 3);
+
+                if (rows[r].contains(value) ||
+                    cols[c].contains(value) ||
+                    boxes[boxIndex].contains(value)) {
+                    return false;
+                }
+
+                rows[r].add(value);
+                cols[c].add(value);
+                boxes[boxIndex].add(value);
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        SudokuValidator validator = new SudokuValidator();
+
+        char[][] board = {
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        System.out.println(validator.isValidSudoku(board));
+    }
+}
