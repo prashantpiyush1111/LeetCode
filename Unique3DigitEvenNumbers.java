8d82e13f852f97e8bda9aede6da68bfb6af8e839
Prashant Maurya <prashantpiyush1111@gmail.com>
2026-09-11 12:40:53 +0530

    Add Unique 3-Digit Even Numbers solution

---
+++ Unique3DigitEvenNumbers.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        boolean[] seen = new boolean[1000];
+        int ans = 0;
+        for (int i = 0; i < digits.length; i++) {
+            for (int j = 0; j < digits.length; j++) {
+                for (int k = 0; k < digits.length; k++) {
+                    if (i == j || j == k || i == k) continue;
+                    if (digits[i] == 0 || digits[k] % 2 != 0) continue;
+                    int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+                    if (!seen[num]) {
+                        seen[num] = true;
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
