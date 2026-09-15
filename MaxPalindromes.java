1f549990cd3b884b6bdbcc321e46994e98318e8d
Prashant Maurya <prashantpiyush1111@gmail.com>
2026-09-15 10:43:28 +0530

    Add Maximum Number of Palindromes solution

---
+++ MaxPalindromes.java
@@ -0,0 +1,18 @@
+class Solution{
+    public int maxPalindromes(String s,int k){
+        int n=s.length();
+        int[] dp=new int[n+1];
+        for(int i=k;i<=n;i++){
+            dp[i]=dp[i-1];
+            if(isPal(s,i-k,i-1)) dp[i]=Math.max(dp[i],dp[i-k]+1);
+            if(i-k>=1&&isPal(s,i-k-1,i-1)) dp[i]=Math.max(dp[i],dp[i-k-1]+1);
+        }
+        return dp[n];
+    }
+    private boolean isPal(String s,int l,int r){
+        while(l<r){
+            if(s.charAt(l++)!=s.charAt(r--)) return false;
+        }
+        return true;
+    }
+}
